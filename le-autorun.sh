1b810c876df40ea89022e69f7b17657f1faf9966
Crypto | Seb <root@cryptoseb.pw>
2020-11-01 16:09:14 -0600

    Updated Script
    1. Removed `service nginx restart` (it fails when not run from root)
    2. Removed SSL verification (buggy and not needed)
    3. Changed default directory to /home/example/.acme.sh instead of /root/.acme.sh/ (just some added security)

--- le-autorun.sh
+++ le-autorun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-ACME_PATH="/root/.acme.sh/"
+ACME_PATH="/home/example/.acme.sh/"
 DOMAIN="example.com"
 DOMAIN_SSL="example.com:443"
 PURPLE='\033[1;35m'
@@ -18,18 +18,9 @@ echo "${LIGHT_BLUE}Auto-renewing LetsEncrypt Certificate for ${LIGHT_GRAY}${DOMA
 "${ACME_PATH}"acme.sh --install-cert -d "${DOMAIN}" --fullchain-file /etc/nginx-acme.sh/fullchain.pem
 "${ACME_PATH}"acme.sh --install-cert -d "${DOMAIN}" --cert-file /etc/nginx-acme.sh/cert.pem
 "${ACME_PATH}"acme.sh --install-cert -d "${DOMAIN}" --key-file /etc/nginx-acme.sh/key.pem
-service nginx restart
 echo " "
 echo " "
 echo "${YELLOW}**-_-_-_-_-_-_-_-_-_-_-_-_-_-_  ${NC}[ acme.sh script by @CryptoSeb ]  ${YELLOW}_-_-_-_-_-_-_-_-_-_-_-_-_-_-**${NC}"
 echo " "
-if true | openssl s_client -connect ${DOMAIN_SSL} 2>/dev/null | \
-  openssl x509 -noout -checkend 0; then
-  echo "${LIGHT_BLUE}Certificate appears to be ${LIGHT_GREEN}VALID! ${LIGHT_BLUE}No further action is required.${NC}"
-else
-  echo "${LIGHT_GRAY}Certificate has ${RED}FAILED ${LIGHT_GRAY}to be installed properly. Please check the above output for errors!${NC}"
-fi
-echo " "
-echo "${LIGHT_GRAY}Your certificate is/was valid from:${NC}"
-echo | openssl s_client -servername ${DOMAIN} -connect ${DOMAIN_SSL} 2>/dev/null | openssl x509 -noout -dates
+echo "${RED}*NOTICE* ${LIGHT_GRAY}YOU NEED TO MANUALLY RESTART NGINX FOR CHANGES TO TAKE EFFECT! ${RED}*NOTICE* ${NC}"
 echo "${YELLOW}**-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-**${NC}"
